c000980f1323f1ab1595cf9f5dc319f041daf0fa
Sebastian Guerrero <sebasagpz@gmail.com>
2025-06-05 17:31:44 -0500

    ACTUALIZACION

---
+++ TALLERES/TallerCondicionales.py
@@ -0,0 +1,97 @@
+# #  Ejercicios con Condicionales y Operaciones Matemáticas
+# # 1. Verifica si un numero es positivo, negativo o cero
+
+
+# print(f"-----------TALLER CONDICIONALES-----------")
+
+# print(f"EJERCICIO 1")
+# n1 = (float(input("Ingresa un numero a verificar: ")))
+
+# if n1 > 0:
+#     print("El numero es positivo")
+# elif n1 < 0:
+#     print("El numero es negativo")
+# elif n1 == 0:
+#     print("El numero es 0")
+# else:
+#     print("Hubo un error")
+
+# print(f"EJERCICIO 2")
+
+# n2 = (float(input("Ingresa un primer numero a verificar: ")))
+# n3 = (float(input("Ingresa un segundo numero a verificar: ")))
+
+# if n2 > n3:
+#     print("El numero", n2, "es mayor a", n3)
+# elif n3 > n2:
+#     print("El numero", n3, "es mayor a", n2)
+# elif n2 < n3:
+#     print("El numero", n2, "es menor a", n3)
+# elif n3 < n2:
+#     print("El numero", n3, "es menor a", n2)
+# else:
+#     print("Este no es un numero válido")
+
+# print(f"EJERCICIO 3")
+
+# n4 = (float(input("Ingresa un numero a verificar: ")))
+
+
+# print(f"EJERCICIO 4")
+
+# n5 = (float(input("Ingresa el numero a verificar: ")))
+
+# if n5 >= 10 and n5 <= 20:
+#     print("Está entre 10 y 20")
+# else:
+#     print("No está entre 10 y 20")
+
+# print(f"EJERCICIO 5")
+
+# n6 = float(input("Ingresa el primer numero: "))
+# n7 = float(input("Ingresa el segundo numero: "))
+# n8 = float(input("Ingresa el tercer numero: "))
+
+# if n6 > n7 and n6 >= n8:
+#     print("El numero", n6, "es mayor")
+# elif n7 > n6 and n7 >= n8:
+#     print("El numero", n7, "es mayor")
+# else:
+#     print("El numero", n8, "es mayor")
+
+# print(f"EJERCICIO 6")
+
+# pfin = (float(input("Ingresa el precio final del producto: ")))
+
+# if pfin > 100:
+#     por = pfin * 0.10
+#     des = pfin - por
+#     print("El total fue de", des)
+# else:
+#     print("No es mayor a 100$")
+    
+# print(f"EJERCICIO 7")
+
+# ed1 = int(input("Bienvenido, para votar debes ingresar tu edad: "))
+
+# if ed1 >= 18:
+#     print("Puedes votar")
+# else:
+#     print("Eres menor de edad, no puedes votar")
+    
+# print(f"EJERCICIO 8")
+
+# vn = (input("Bienvenido Usuario, ¿Eres VIP o NORMAL?: ")).upper()
+# n9 = (float(input("Ingresa el precio del producto: ")))
+
+# if vn == "VIP":
+#     por1 = n9 * 0.20
+#     des2 = n9 - por1
+#     print("Bienvenido cliente VIP, su precio es de", des2)
+# else:
+#     print("Bienvenido cliente NORMAL, su precio es de", n9)
+
+# #  Ejercicios con Condicionales y Operaciones Matemáticas
+# # 2. Ejercicios con Listas (con condicionales)
+
+lista1 = [2, 5, 8, 9, 3]
